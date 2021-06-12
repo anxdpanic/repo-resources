e0e619ddbe8d5fcf19decd75b41a39c025acdabf
anxdpanic <anxdpanic@users.noreply.github.com>
2021-06-12 13:44:25 -0400

    no message

---
+++ increment_version.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+
+GET_VERSION = re.compile(r'''<addon.+?version="(?P<version>[0-9.]+)"''', re.DOTALL)
+
+
+def increment_version(version):
+    version = version.split('.')
+    version[2] = str(int(version[2]) + 1)
+    return '.'.join(version)
+
+
+if __name__ == '__main__':
+    changed_files = sys.argv
+    if not changed_files:
+        exit(0)
+
+    language_folders = []
+    for changed_file in changed_files:
+        if changed_file.startswith('resource.language') and \
+                changed_file.endswith(('strings.po', 'langinfo.xml')):
+            folder = os.path.split(changed_file)[0]
+            language_folders.append(folder.split('/')[0])
+
+    print('Files were modified in the following languages:')
+    for language_folder in language_folders:
+        print('\t{language}'.format(language=language_folder))
+
+    print('')
+
+    addon_xmls = ['/'.join([folder, 'addon.xml']) for folder in language_folders]
+
+    for addon_xml in addon_xmls:
+        print('Reading {filename}'.format(filename=addon_xml))
+        with open(addon_xml, 'r') as open_file:
+            xml_content = open_file.read()
+
+        version_match = GET_VERSION.search(xml_content)
+        if not version_match:
+            print('Unable to determine current version... skipping.', '')
+            continue
+
+        old_version = version_match.group('version')
+        new_version = increment_version(old_version)
+        print('\tOld Version: {version}'.format(version=old_version))
+        print('\tNew Version: {version}'.format(version=new_version))
+
+        new_xml_content = xml_content.replace(
+            'version="{version}"'.format(version=old_version),
+            'version="{version}"'.format(version=new_version),
+        )
+
+        if xml_content == new_xml_content:
+            print('XML was unmodified... skipping.', '')
+            continue
+
+        print('Writing {filename}'.format(filename=addon_xml))
+        with open(addon_xml, 'w') as open_file:
+            open_file.write(xml_content)
+
+        print('')
+
+    print('')
