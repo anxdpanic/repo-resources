436fcd2086bf397703d5d818e317e3e600c5062b
anxdpanic <anxdpanic@users.noreply.github.com>
2021-06-12 13:03:25 -0400

    no message

--- version_bump.py
+++ version_bump.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
 import sys
 
 if __name__ == '__main__':
-    print('a' * 10, sys.argv)
+    changed_files = sys.argv
+    if not changed_files:
+        exit(0)
+
+    language_folders = []
+    for changed_file in changed_files:
+        if changed_file.startswith('resource.language') and \
+                changed_file.endswith(('strings.po', 'langinfo.xml')):
+            folder = os.path.split(changed_file)[0]
+            language_folders.append(folder.split(os.sep)[0])
+
+    print('a' * 10, language_folders)
